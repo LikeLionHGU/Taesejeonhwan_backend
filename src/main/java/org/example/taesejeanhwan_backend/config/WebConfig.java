7ea79c0453413b045eba15def64d2ba92cb60fea
junhyeok0821 <choi20060821@gmail.com>
2026-02-11 22:11:48 +0900

    login test complete

---
+++ src/main/java/org/example/taesejeanhwan_backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.example.taesejeanhwan_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
